656c594ea29563602793c593e4cc785eb23893e9
Yutaka Kamioka <yutaka.kamioka@jp.fujitsu.com>
2025-08-22 14:15:03 +0900

    Added: -k/--insecure option

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"crypto/tls"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [URL]\n", os.Args[0])
+	// コマンドラインフラグを定義
+	insecure := flag.Bool("k", false, "allow insecure server connections when using SSL")
+	flag.BoolVar(insecure, "insecure", false, "allow insecure server connections when using SSL")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] [URL]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	// URLを取得
+	args := flag.Args()
+	if len(args) != 1 {
+		flag.Usage()
 		os.Exit(1)
 	}
+	url := args[0]
 
-	url := os.Args[1]
+	// HTTPトランスポートを作成
+	transport := &http.Transport{}
+
+	// insecureオプションが指定されている場合、TLS証明書の検証をスキップ
+	if *insecure {
+		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	}
 
 	// HTTPクライアントを作成（リダイレクト無効）
 	client := &http.Client{
+		Transport: transport,
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
